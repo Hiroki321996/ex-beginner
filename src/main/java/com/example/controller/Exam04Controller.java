a9d00e353cfa6c3ccc391290778218ea3e07b622
Hiroki321996 <hiroki.tani@rakus-partners.co.jp>
2020-02-04 16:22:31 +0900

    コントローラーを作成しました

---
+++ src/main/java/com/example/controller/Exam04Controller.java
@@ -0,0 +1,47 @@
+package com.example.controller;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.form.Ex04Form;
+
+@Controller
+@RequestMapping("/ex04")
+public class Exam04Controller {
+	
+	@Autowired
+	private ServletContext application;
+	
+	
+	
+	@ModelAttribute
+	public Ex04Form setUpForm() {
+		return new Ex04Form();
+	}
+	
+	@RequestMapping("")
+	public String index() {
+		return "exam04";
+	}
+	
+	@RequestMapping("/self-introduction")
+	public String selfIntroduction(@Validated Ex04Form form,
+			BindingResult result,String name,String age,String comment
+			) {
+		
+		if(result.hasErrors()) {
+			return index();
+		}
+		application.setAttribute("name", name);
+		application.setAttribute("age", age);
+		application.setAttribute("comment", comment);
+		
+		return "exam04-result";
+	}
+}
